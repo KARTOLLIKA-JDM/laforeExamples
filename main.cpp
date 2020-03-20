56da55e7815dc38bdc919937b6eaaec720c1c7af
KARTOLLIKA-JDM <m_svalov@mail.ru>
2020-03-20 12:10:48 +0500

    Сделал ввод переменных в main, а рассчеты в функции calculator

--- main.cpp
+++ main.cpp
@@ -4,30 +4,35 @@
 using namespace std;
 
 
-void calculator ();
+void calculator (float number1, float number2, char operation);
 
-int main() {
+int main()
+{
     system("chcp 65001");
-    calculator ();
+    float number1 = 0;
+    float number2 = 0;
+    char operation = 'b';
+    char answer='a';
+    while (answer != 'n')
+        {
+        cout << "введите первое число: ";
+        cin >> number1;
+        cout << "введите операцию(+,-,*,/): ";
+        cin >> operation;
+        cout << "введите второе число: ";
+        cin >> number2;
+        calculator(number1, number2, operation);
+        cout << "Продолжить?(y/n)  ";
+        cin >> answer;
+    }
         return 0;
 }
 
 
-void calculator()
+void calculator(float number1, float number2, char operation)
 {
-    float number1;
-    float number2;
-    char operation;
-    char answer='a';
-    while (answer != 'n')
-    {
-    cout << "введите первое число: ";
-    cin >> number1;
-    cout << "введите операцию(+,-,*,/): ";
-    cin >> operation;
-    cout << "введите второе число: ";
-    cin >> number2;
-        switch (operation) {
+        switch (operation)
+        {
             case '+':
                 cout << "сумма равна: " << number1 + number2 << endl;
                 break;
@@ -43,8 +48,5 @@ void calculator()
             default:
                 cout << "неверное действие, повторите попытку...\n";
         }
-        cout << "Продолжить?(y/n)  ";
-        cin >> answer;
-    }
 }
 
